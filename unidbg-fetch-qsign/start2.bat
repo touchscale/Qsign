6dcddcd7f09021f2053a586f1f51c95a9bd42aae
batvbs <batvbs@qq.com>
2023-11-22 15:39:17 +0000

    up
    Signed-off-by: batvbs <batvbs@qq.com>

---
+++ unidbg-fetch-qsign/start2.bat
@@ -0,0 +1,9 @@
+@echo 如果运行后乱码，请右键编辑脚本 另存为 编码选ANSI 保存
+@ echo off
+title API请勿关闭
+echo API已启动，日志输出log.txt
+rem 劫持控制台输出到文件，1为正常消息，2为错误消息，34为缓冲区
+rem 使用findstr命令筛选需要的日志消息
+>>log.txt 2>&1 3>&1 4>&1 echo %date%-%time%
+bin\unidbg-fetch-qsign --basePath=txlib\8.9.85|findstr "警告:"
+exit
